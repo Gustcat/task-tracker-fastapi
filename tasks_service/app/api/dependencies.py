2e1ddd63518e647726a4c308164d1264e2a8eb19
Екатерина Густова <antinpanic@gmail.com>
2025-10-15 16:19:23 +0300

    refactor(tasks_service): transfer files

--- tasks_service/app/api/dependencies.py
+++ tasks_service/app/api/dependencies.py
@@ -4,7 +4,7 @@ from typing_extensions import Annotated
 
 from app.repository.tasks import TaskRepository
 from app.service.tasks import TaskService
-from db.db import get_async_session
+from app.db.db import get_async_session
 
 
 def get_task_service(session: Annotated[AsyncSession, Depends(get_async_session)]):
